425d6f0b8fcc3cfefff2836e45f956adda98a5ec
REIS0 <aor@cesar.school>
2020-07-29 01:54:24 -0300

    rework installation script

--- amp/install_plugin.sh
+++ amp/install_plugin.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+mkdir -p target/lv2
 cp -r eg-amp-rs.lv2/ target/lv2/
-cp target/x86_64-unknown-linux-gnu/release/libamp.so target/lv2/eg-amp-rs.lv2/
+cp target/debug/libamp.so target/lv2/eg-amp-rs.lv2/
 cp -r target/lv2/* ~/.lv2/
